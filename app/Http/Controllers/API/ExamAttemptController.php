407205006e80ae8d31668776a939614a0ab64f97
frmnxhken <frmnxhken@gmail.com>
2026-06-11 21:43:20 +0700

    Refacto ExamAttempt

--- app/Http/Controllers/API/ExamAttemptController.php
+++ app/Http/Controllers/API/ExamAttemptController.php
@@ -3,178 +3,35 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use App\Http\Resources\ExamQuestionResource;
 use App\Models\ExamAssignment;
-use App\Models\ExamAttempt;
-use App\Models\Grade;
-use App\Models\QuestionOption;
+use App\Services\ExamAttemptService;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class ExamAttemptController extends Controller
 {
+    public function __construct(private ExamAttemptService $service) {}
+
     public function index($id_class_subject, ExamAssignment $exam)
     {
-        $user = Auth::user();
-
-        return ExamAttempt::where([
-            'exam_assignment_id' => $exam->id,
-            'student_id' => $user->student->id,
-        ])
-            ->with([
-                'assignment.exam.subject'
-            ])
-            ->firstOrFail();
+        $attempt = $this->service->getAttempt($exam);
+        return response()->json($attempt);
     }
 
     public function store(Request $request, $id_class_subject, ExamAssignment $exam)
     {
-        $user = Auth::user();
-
-        $attempt = ExamAttempt::where([
-            'exam_assignment_id' => $exam->id,
-            'student_id' => $user->student->id,
-        ])->first();
-
-        if (!$attempt->started_at) {
-            $attempt->update([
-                'started_at' => now(),
-                'status' => 'in_progress'
-            ]);
-        }
+        $start = $this->service->startAttempt($exam);
+        return response()->json($start);
     }
 
     public function show(Request $request, $id_class_subject, ExamAssignment $exam)
     {
-        $user = Auth::user();
-
-
-        if ($exam->class_subject_id != $id_class_subject) {
-            return response()->json([
-                'message' => 'Exam not found'
-            ], 404);
-        }
-
-        $attempt = ExamAttempt::where([
-            'exam_assignment_id' => $exam->id,
-            'student_id' => $user->student->id,
-        ])->first();
-
-        if (!$attempt) {
-            return response()->json([
-                'message' => 'You are not assigned to this exam'
-            ], 403);
-        }
-
-        if (now()->lt($exam->start_time)) {
-            return response()->json([
-                'message' => 'Exam has not started yet'
-            ], 403);
-        }
-
-        if (!$attempt->started_at) {
-            return response()->json([
-                'message' => 'Exam has not started yet'
-            ], 403);
-        }
-
-        if (now()->gt($exam->end_time)) {
-            return response()->json([
-                'message' => 'Exam has ended'
-            ], 403);
-        }
-
-        if ($attempt->status === 'submitted') {
-            return response()->json([
-                'message' => 'Exam already submitted'
-            ], 403);
-        }
-
-
-        $questions = $exam
-            ->exam
-            ->questions()
-            ->with('options')
-            ->get();
-
-        return response()->json([
-            'attempt_id' => $attempt->id,
-            'started_at' => $attempt->started_at,
-            'end_time' => $exam->end_time,
-            'duration' => $exam->exam->duration,
-            'type' => $exam->exam->type,
-            'subject' => $exam->exam->subject->name,
-            'questions' => ExamQuestionResource::collection($questions),
-        ]);
+        $examSession = $this->service->getExamSession($exam);
+        return response()->json($examSession);
     }
 
     public function attempt(Request $request, $id_class_subject, ExamAssignment $exam)
     {
-        $user = Auth::user();
-
-        $attempt = ExamAttempt::where([
-            'exam_assignment_id' => $exam->id,
-            'student_id' => $user->student->id,
-        ])->firstOrFail();
-
-        if ($attempt->status == 'submitted') {
-            return response()->json([
-                'message' => 'Exam already submitted'
-            ], 403);
-        }
-
-        $correctAnswers = 0;
-
-        foreach ($request['answers'] as $answer) {
-            $isCorrect = QuestionOption::where([
-                'id' => $answer['option_id'],
-                'is_correct' => true,
-            ])->exists();
-
-            if ($isCorrect) {
-                $correctAnswers++;
-            }
-        }
-
-        $totalQuestions = $exam->exam
-            ->questions()
-            ->count();
-
-        $score = $totalQuestions > 0
-            ? (100 / $totalQuestions) * $correctAnswers
-            : 0;
-
-        $attempt->update([
-            'score' => round($score, 2),
-            'submitted_at' => now(),
-            'status' => 'submitted',
-        ]);
-
-        $grade = Grade::where('student_id', $user->student->id)->firstOrFail();
-
-        switch ($exam->exam->type) {
-            case 'uas':
-                $grade->update([
-                    'uas_score' => $score
-                ]);
-                break;
-            case 'uts':
-                $grade->update([
-                    'uts_score' => $score
-                ]);
-                break;
-            case 'harian':
-                $grade->update([
-                    'daily_total_score' =>
-                    $grade->daily_total_score + $score
-                ]);
-                break;
-        }
-
-        return response()->json([
-            'score' => round($score, 2),
-            'correct_answers' => $correctAnswers,
-            'ex' => $exam->exam->type,
-        ]);
+        $submit = $this->service->submitAttempt($exam, $request->answers);
+        return response()->json($submit);
     }
 }
--- app/Services/ExamAttemptService.php
+++ app/Services/ExamAttemptService.php
@@ -2,13 +2,16 @@
 
 namespace App\Services;
 
+use App\Http\Resources\ExamQuestionResource;
 use App\Models\ExamAttempt;
 use App\Models\Grade;
+use App\Models\QuestionOption;
 use App\Models\Student;
+use Illuminate\Support\Facades\Auth;
 
 class ExamAttemptService
 {
-    public function generate(int $assignmentId, int $classSubjectId): void
+    public function generate($assignmentId, $classSubjectId)
     {
         $students = Student::whereHas(
             'enrollments.schoolClass.classSubjects',
@@ -30,7 +33,7 @@ class ExamAttemptService
         ExamAttempt::insert($data);
     }
 
-    public function rollbackFromAttempts($assignment, $examType): void
+    public function rollbackFromAttempts($assignment, $examType)
     {
         $attempts = ExamAttempt::where('exam_assignment_id', $assignment->id)->get();
 
@@ -54,5 +57,120 @@ class ExamAttemptService
                     break;
             }
         }
+    }
+
+    public function getAttempt($exam)
+    {
+        $user = Auth::user();
+        $attempt = ExamAttempt::where([
+            'exam_assignment_id' => $exam->id,
+            'student_id' => $user->student->id,
+        ])->with('assignment.exam.subject')->firstOrFail();
+
+        return $attempt;
+    }
+
+    public function startAttempt($exam)
+    {
+        $attempt = $this->getAttempt($exam);
+
+        if (!$attempt->started_at) {
+            $attempt->update([
+                'started_at' => now(),
+                'status'     => 'in_progress'
+            ]);
+        }
+
+        return $attempt;
+    }
+
+    public function getExamSession($exam)
+    {
+        $this->validateExamWindow($exam);
+        $attempt = $this->getAttempt($exam);
+
+        $this->validateAttempt($attempt);
+        $questions = $exam->exam->questions()->with('options')->get();
+
+        $data =  [
+            'attempt_id' => $attempt->id,
+            'started_at' => $attempt->started_at,
+            'end_time'   => $exam->end_time,
+            'duration'   => $exam->exam->duration,
+            'type'       => $exam->exam->type,
+            'subject'    => $exam->exam->subject->name,
+            'questions'  => ExamQuestionResource::collection($questions),
+        ];
+
+        return $data;
+    }
+
+    public function submitAttempt($exam, $answers)
+    {
+        $user = Auth::user();
+        $attempt = $this->getAttempt($exam);
+
+        if ($attempt->status === 'submitted') {
+            abort(403, 'Already submitted');
+        }
+
+        $correct = $this->calculateScore($answers);
+        $total = $exam->exam->questions()->count();
+
+        $score = $total > 0 ? (100 / $total) * $correct : 0;
+
+        $attempt->update([
+            'score'        => round($score, 2),
+            'submitted_at' => now(),
+            'status'       => 'submitted',
+        ]);
+
+        $this->updateGrade($exam, $user, $score);
+
+        return [
+            'score'           => round($score, 2),
+            'correct_answers' => $correct,
+        ];
+    }
+
+    private function calculateScore($answers)
+    {
+        $correct = 0;
+
+        foreach ($answers as $answer) {
+            $isCorrect = QuestionOption::where([
+                'id' => $answer['option_id'],
+                'is_correct' => true,
+            ])->exists();
+
+            if ($isCorrect) $correct++;
+        }
+
+        return $correct;
+    }
+
+    private function updateGrade($exam, $user, $score)
+    {
+        $grade = Grade::where('student_id', $user->student->id)->firstOrFail();
+
+        match ($exam->exam->type) {
+            'uas' => $grade->update(['uas_score' => $score]),
+            'uts' => $grade->update(['uts_score' => $score]),
+            'harian' => $grade->update([
+                'daily_total_score' => $grade->daily_total_score + $score
+            ]),
+        };
+    }
+
+    private function validateExamWindow($exam): void
+    {
+        if (now()->lt($exam->start_time)) abort(403, 'Not started');
+        if (now()->gt($exam->end_time)) abort(403, 'Ended');
+    }
+
+    private function validateAttempt($attempt): void
+    {
+        if (!$attempt->started_at) abort(403, 'Not started');
+        if ($attempt->status === 'submitted') abort(403, 'Already submitted');
     }
 }
